75c4588912c26d14b13f1b58abbdccdc027ee061
Vasile Drumea <drumeavasilexd@gmail.com>
2019-01-05 22:58:27 +0200

    Add tweet functionalities

--- tviter/app/Controllers/Http/TweetController.js
+++ tviter/app/Controllers/Http/TweetController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Tweet = use('App/Models/Tweet')
+const Reply = use('App/Models/Reply')
 
 class TweetController {
     async tweet ({ request, auth, response }) {
@@ -20,6 +21,71 @@ class TweetController {
             status: 'success',
             message: 'Tweet posted!',
             data: tweet
+        })
+    }
+
+    async show ({ params, response }) {
+        try {
+            const tweet = await Tweet.query()
+                .where('id', params.id)
+                .with('user')
+                .with('replies')
+                .with('replies.user')
+                .with('favorites')
+                .firstOrFail()
+    
+            return response.json({
+                status: 'success',
+                data: tweet
+            })
+        } catch (error) {
+            return response.status(404).json({
+                status: 'error',
+                message: 'Tweet not found'
+            })
+        }
+    }
+
+    async reply ({ request, auth, params, response }) {
+        // get currently authenticated user
+        const user = auth.current.user
+    
+        // get tweet with the specified ID
+        const tweet = await Tweet.find(params.id)
+    
+        // persist to database
+        const reply = await Reply.create({
+            user_id: user.id,
+            tweet_id: tweet.id,
+            reply: request.input('reply')
+        })
+    
+        // fetch user that made the reply
+        await reply.load('user')
+    
+        return response.json({
+            status: 'success',
+            message: 'Reply posted!',
+            data: reply
+        })
+    }
+
+    async destroy ({ request, auth, params, response }) {
+        // get currently authenticated user
+        const user = auth.current.user
+    
+        // get tweet with the specified ID
+        const tweet = await Tweet.query()
+            .where('user_id', user.id)
+            .where('id', params.id)
+            .firstOrFail()
+    
+        await tweet.delete()
+    
+        return response.json({
+            status: 'success',
+            message: 'Tweet deleted!',
+            data: null
         })
     }
 }
